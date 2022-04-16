31b4fa6ac59339586604a390a031a90b76990d84
Your Name <you@example.com>
2022-04-16 09:27:18 +0500

    a

--- sag/settings.py
+++ sag/settings.py
@@ -136,6 +136,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # AUTH_USER_MODEL = 'mainapp.User'
 
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
